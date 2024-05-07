9f5b5c9a6497c36e0013186d80979bf50e051376
jaimin25 <jaiminchovatia25123@gmail.com>
2024-05-07 10:02:18 +0530

    fix: responsive feature cards

--- src/components/Home/FeatureSection/feature-section.tsx
+++ src/components/Home/FeatureSection/feature-section.tsx
@@ -6,7 +6,7 @@ export default function FeatureSection() {
   return (
     <div className="flex-1 bg-white p-8">
       <div>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
           {new Array(3).fill(0).map((_, i) => (
             <AuctionCards key={i} />
           ))}
