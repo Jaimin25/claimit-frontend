c6322ef2a81baa335f4482bb9579f0d80b712b43
jaimin25 <jaiminchovatia25123@gmail.com>
2024-05-08 15:45:56 +0530

    fix: transactions table in wallet in dashboard

--- src/components/Dashboard/wallet.tsx
+++ src/components/Dashboard/wallet.tsx
@@ -3,6 +3,15 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Button } from '../ui/button';
@@ -58,7 +67,7 @@ export default function Wallet() {
         <CardHeader>
           <h3 className="text-3xl font-semibold">Wallet</h3>
         </CardHeader>
-        <CardContent className="space-y-4">
+        <CardContent className="space-y-8">
           <div>
             <h2 className="text-xl font-semibold">Provide Some Proofs</h2>
             <p className="text-sm text-gray-500">
@@ -131,6 +140,40 @@ export default function Wallet() {
               <Button variant={'default'}>Add</Button>
               <Button variant={'secondary'}>Withdraw</Button>
             </div>
+          </div>
+          <div>
+            <h2 className="text-xl font-semibold">Transactions</h2>
+            <Table>
+              <TableCaption>A list of your transacntions.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Type</TableHead>
+                  <TableHead>ID</TableHead>
+                  <TableHead>Amount</TableHead>
+                  <TableHead>Time</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array(10)
+                  .fill(0)
+                  .map((_, i) => (
+                    <TableRow key={i}>
+                      <TableCell className="font-medium">
+                        {i % 2 === 0 ? 'Deposit' : 'Withdraw'}
+                      </TableCell>
+                      <TableCell>ipx_f24n234Dh2</TableCell>
+                      <TableCell>
+                        {i % 2 === 0 ? (
+                          <p className="text-green-500">+{i * 250}</p>
+                        ) : (
+                          <p className="text-red-500">-{i * 500}</p>
+                        )}
+                      </TableCell>
+                      <TableCell>10 May, 2024 10:09:00 PM</TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
           </div>
         </CardContent>
       </Card>
