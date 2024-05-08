96bc19aaea7bc7062bf6e0fe4cc7ec73b5da6c98
jaimin25 <jaiminchovatia25123@gmail.com>
2024-05-08 21:46:28 +0530

    fix: separate mobile nav component dashboard

--- src/app/(main)/(Dashboard)/dashboard/layout.tsx
+++ src/app/(main)/(Dashboard)/dashboard/layout.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import Link from 'next/link';
-import { FaChevronDown } from 'react-icons/fa';
-import { IoMdList } from 'react-icons/io';
-import { MdAccountBalanceWallet, MdAccountCircle } from 'react-icons/md';
-import { RiAuctionFill } from 'react-icons/ri';
 
+import MobileNav from '@/components/Dashboard/mobile-nav';
 import Sidebar from '@/components/Dashboard/sidebar';
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarTrigger,
-} from '@/components/ui/menubar';
+
 export default function RootLayout({
   children,
 }: {
@@ -24,39 +14,7 @@ export default function RootLayout({
         <Sidebar />
       </div>
       <div className="block w-full p-6 lg:hidden">
-        <Menubar>
-          <MenubarMenu>
-            <MenubarTrigger>
-              Navigation <FaChevronDown className="mx-2" />
-            </MenubarTrigger>
-            <MenubarContent>
-              <Link href={'/dashboard/accountdetails'}>
-                <MenubarItem className="flex items-center gap-2">
-                  <MdAccountCircle size={24} />
-                  Account Details
-                </MenubarItem>
-              </Link>
-              <Link href={'/dashboard/wallet'}>
-                <MenubarItem className="flex items-center gap-2">
-                  <MdAccountBalanceWallet size={24} />
-                  Wallet
-                </MenubarItem>
-              </Link>
-              <Link href={'/dashboard/myauctions'}>
-                <MenubarItem className="flex items-center gap-2">
-                  <RiAuctionFill size={24} />
-                  My Auctions
-                </MenubarItem>
-              </Link>
-              <Link href={'/dashboard/mybids'}>
-                <MenubarItem className="flex items-center gap-2">
-                  <IoMdList size={24} />
-                  My Bids
-                </MenubarItem>
-              </Link>
-            </MenubarContent>
-          </MenubarMenu>
-        </Menubar>
+        <MobileNav />
       </div>
       <div className="children-container h-full w-full p-6 lg:w-5/6">
         {children}
