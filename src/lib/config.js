72361dfa57b99cda945cbb6b6cc1b08495815431
jaimin25 <jaiminchovatia25123@gmail.com>
2024-05-15 22:18:37 +0530

    fix: api url hosted

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,9 +1,11 @@
 export const Config = {
   APP_NAME: 'Claimit',
   APP_URL:
-    process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '',
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://claimit.vercel.app',
   API_URL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:8000/api/v1'
-      : 'https://claimit-backend.onrender.com/',
+      : 'https://claimit-backend.onrender.com',
 };
