e1bf979d3560339030a3d8069a8d3d86341c7c84
jaimin25 <jaiminchovatia25123@gmail.com>
2024-05-16 13:18:05 +0530

    fix: api error happening

--- src/app/api/user/updateUserData/route.ts
+++ src/app/api/user/updateUserData/route.ts
@@ -12,33 +12,44 @@ export async function POST(req: NextRequest) {
   const formData = await req.formData();
   const file = formData.get('file') as File;
 
-  const newFd = new FormData();
-  newFd.append('document', formData.get('document') as string);
-  let filepath;
-  if (file && typeof file !== 'string') {
-    const ab = await file.arrayBuffer();
-    const bf = Buffer.from(ab);
-    filepath = path.join(cwd, '.temp/upload', file.name) + Date.now();
-    await fs.promises.writeFile(filepath, bf, {
-      encoding: 'binary',
+  try {
+    const newFd = new FormData();
+    newFd.append('document', formData.get('document') as string);
+    let filepath;
+    if (file && typeof file !== 'string') {
+      const ab = await file.arrayBuffer();
+      const bf = Buffer.from(ab);
+      filepath = path.join(cwd, '.temp/upload', file.name) + Date.now();
+      await fs.promises.writeFile(filepath, bf, {
+        encoding: 'binary',
+      });
+
+      newFd.append('file', fs.createReadStream(filepath));
+    }
+
+    const res = await axios.post(
+      `${Config.API_URL}/user/updateUserData`,
+      newFd,
+      {
+        withCredentials: true,
+        headers: {
+          cookie: `session=${req.cookies.get('session')?.value}`,
+          'Content-Type': req.headers.get('content-type'),
+        },
+      }
+    );
+
+    const resData = await res.data;
+
+    if (resData && filepath && fs.existsSync(filepath)) {
+      fs.unlinkSync(filepath);
+    }
+    return NextResponse.json(resData);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      statuCode: 404,
+      statusMessage: (error as Error).stack,
     });
-
-    newFd.append('file', fs.createReadStream(filepath));
   }
-
-  const res = await axios.post(`${Config.API_URL}/user/updateUserData`, newFd, {
-    withCredentials: true,
-    headers: {
-      cookie: `session=${req.cookies.get('session')?.value}`,
-      'Content-Type': req.headers.get('content-type'),
-    },
-  });
-
-  const resData = await res.data;
-
-  if (resData && filepath && fs.existsSync(filepath)) {
-    fs.unlinkSync(filepath);
-  }
-
-  return NextResponse.json(resData);
 }
