64fec2206265a8e5f5160d97890aee133724f435
jaimin25 <jaiminchovatia25123@gmail.com>
2024-05-16 14:54:24 +0530

    fix: json not formdata profile updates

--- src/app/api/user/updateUserData/route.ts
+++ src/app/api/user/updateUserData/route.ts
@@ -4,8 +4,6 @@ import FormData from 'form-data';
 
 import { Config } from '@/lib/config';
 
-// const cwd = process.cwd();
-
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
   const imgFileBase64 = formData.get('profileImgFileBase64') as File;
@@ -20,42 +18,23 @@ export async function POST(req: NextRequest) {
         formData.get('profileImgFileBase64') as string
       );
     }
-    // let filepath;
-    // if (file && typeof file !== 'string') {
-    //   const uploadImage = await axios.post(
-    //     `https://upload.imagekit.io/api/v1/files/upload`,
-    //     formData
-    //   );
-
-    //   console.log(uploadImage);
-    //   const ab = await file.arrayBuffer();
-    //   const bf = Buffer.from(ab);
-    //   filepath = join('/', 'tmp', file.name) + Date.now();
-    //   console.log(filepath);
-    //   await fs.promises.writeFile(filepath, bf, {
-    //     encoding: 'binary',
-    //   });
-
-    //   newFd.append('file', fs.createReadStream(filepath));
-    // }
 
     const res = await axios.post(
       `${Config.API_URL}/user/updateUserData`,
-      newFd,
+      {
+        document: formData.get('document') as string,
+        profileImgFileBase64: formData.get('profileImgFileBase64') as string,
+      },
       {
         withCredentials: true,
         headers: {
           cookie: `session=${req.cookies.get('session')?.value}`,
-          'Content-Type': req.headers.get('content-type'),
         },
       }
     );
 
     const resData = await res.data;
 
-    // if (resData && filepath && fs.existsSync(filepath)) {
-    //   fs.unlinkSync(filepath);
-    // }
     return NextResponse.json(resData);
   } catch (error) {
     console.log(error);
