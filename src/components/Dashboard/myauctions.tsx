21c52adbf3f9857c78eac23b767740fc8f52edc4
jaimin25 <jaiminchovatia25123@gmail.com>
2024-05-20 14:21:11 +0530

    feat: display user auctions in dashboard

--- src/components/Dashboard/myauctions.tsx
+++ src/components/Dashboard/myauctions.tsx
@@ -1,10 +1,60 @@
+'use client';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { FaSearch } from 'react-icons/fa';
+import { toast } from 'sonner';
+
+import { Config } from '@/lib/config';
+import { useMutation } from '@tanstack/react-query';
 
 import DashboardAuctionCard from '../Cards/dashboard-auction-card';
 import { Card, CardContent, CardHeader } from '../ui/card';
 import { Input } from '../ui/input';
 
+export interface UserAuctionsProps {
+  id: string;
+  title: string;
+  description: string;
+  imagesUrl: string[];
+  buyPrice: string;
+  basePrice: string;
+  auctionStatus: string;
+  startingDate: string;
+  endingDate: string;
+  city: string;
+  state: string;
+  country: string;
+  bids: {
+    id: number;
+  }[];
+  createdAt: Date;
+}
+
+const fetchUserAuctions = async () => {
+  return await axios.get(`${Config.APP_URL}/api/user/getUserAuctionsById`);
+};
+
 export default function MyAuctions() {
+  const [userAuctions, setUserAuctions] = useState<UserAuctionsProps[]>();
+
+  const fetchUserAuctionsMutation = useMutation({
+    mutationFn: fetchUserAuctions,
+    onSuccess: async (res) => {
+      const data = await res.data;
+
+      if (data.statusCode === 200) {
+        setUserAuctions(data.userAuctions as UserAuctionsProps[]);
+      }
+    },
+    onError: (error) => toast.error(`${error.name}: ${error.message}`),
+  });
+
+  useEffect(() => {
+    fetchUserAuctionsMutation.mutate();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="h-full w-full">
       <Card className="h-full">
@@ -17,11 +67,9 @@ export default function MyAuctions() {
             <Input placeholder="Search auctions..." />
           </div>
           <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-            {Array(5)
-              .fill(0)
-              .map((_, i) => (
-                <DashboardAuctionCard key={i} />
-              ))}
+            {userAuctions?.map((item, i) => (
+              <DashboardAuctionCard key={i} auction={item} />
+            ))}
           </div>
         </CardContent>
       </Card>
