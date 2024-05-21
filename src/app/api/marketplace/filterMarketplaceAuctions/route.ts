4080d34e463d3d9366d0e28345ea8962d52189fa
jaimin25 <jaiminchovatia25123@gmail.com>
2024-05-21 21:06:52 +0530

    feat: rate limiter

--- src/app/api/marketplace/filterMarketplaceAuctions/route.ts
+++ src/app/api/marketplace/filterMarketplaceAuctions/route.ts
@@ -9,24 +9,30 @@ export async function GET(req: NextRequest) {
   const category = searchParams.get('category');
   const sortTypeAuction = searchParams.get('sortTypeAuction');
   const sortTypePrice = searchParams.get('sortTypePrice');
+  try {
+    const res = await axios.get(
+      `${Config.API_URL}/marketplace/filterMarketplaceAuctions`,
+      {
+        withCredentials: true,
+        headers: {
+          cookie: `session=${req.cookies.get('session')?.value}`,
+        },
+        params: {
+          searchInput,
+          category,
+          sortTypeAuction,
+          sortTypePrice,
+        },
+      }
+    );
 
-  const res = await axios.get(
-    `${Config.API_URL}/marketplace/filterMarketplaceAuctions`,
-    {
-      withCredentials: true,
-      headers: {
-        cookie: `session=${req.cookies.get('session')?.value}`,
-      },
-      params: {
-        searchInput,
-        category,
-        sortTypeAuction,
-        sortTypePrice,
-      },
-    }
-  );
+    const resData = await res.data;
 
-  const resData = await res.data;
-
-  return NextResponse.json(resData);
+    return NextResponse.json(resData);
+  } catch (err) {
+    return NextResponse.json({
+      statusCode: 500,
+      statusMessage: (err as Error).message,
+    });
+  }
 }
