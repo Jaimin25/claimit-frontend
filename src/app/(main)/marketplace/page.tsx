c62b13bd5559bb8b4535e68ad9dc443bd41d70ea
jaimin25 <jaiminchovatia25123@gmail.com>
2024-05-27 16:06:14 +0530

    fix: suspense boundry

--- src/app/(main)/marketplace/page.tsx
+++ src/app/(main)/marketplace/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Suspense } from 'react';
 
 import Marketplace from '@/components/Marketplace/marketplace';
 import Sidebar from '@/components/Marketplace/sidebar';
@@ -6,15 +6,17 @@ import { MarketplaceAuctionsProvider } from '@/components/Providers/marketplace-
 
 export default function MarketplacePage() {
   return (
-    <MarketplaceAuctionsProvider>
-      <div className="flex w-full flex-col gap-8 p-8 lg:flex-row">
-        <div className="w-full lg:w-2/6">
-          <Sidebar />
+    <Suspense>
+      <MarketplaceAuctionsProvider>
+        <div className="flex w-full flex-col gap-8 p-8 lg:flex-row">
+          <div className="w-full lg:w-2/6">
+            <Sidebar />
+          </div>
+          <div className="w-full">
+            <Marketplace />
+          </div>
         </div>
-        <div className="w-full">
-          <Marketplace />
-        </div>
-      </div>
-    </MarketplaceAuctionsProvider>
+      </MarketplaceAuctionsProvider>
+    </Suspense>
   );
 }
