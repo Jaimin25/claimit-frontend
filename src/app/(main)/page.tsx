31588adecb39d5ede67fd6326b35dab664b8d246
jaimin25 <chovatiajaimin@gmail.com>
2024-11-13 23:36:54 +0530

    fix: home page hero section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,14 @@
+import { AnimatedListDemo } from '@/components/AnimatedNotifs';
 import FeatureSection from '@/components/Home/FeatureSection/feature-section';
 import HeroSection from '@/components/Home/HeroSection/hero-section';
 
 export default function HomePage() {
   return (
-    <div className="h-full flex-col">
+    <div className="relative flex h-full flex-col">
       <HeroSection />
+      <div className="absolute right-0 top-0 z-50 hidden xl:block">
+        <AnimatedListDemo />
+      </div>
       <FeatureSection />
     </div>
   );
--- src/components/Home/HeroSection/hero-section.tsx
+++ src/components/Home/HeroSection/hero-section.tsx
@@ -1,72 +1,67 @@
-import Link from 'next/link';
-import { TrendingUp } from 'lucide-react';
-import { FaUser } from 'react-icons/fa';
-import { GiConfirmed } from 'react-icons/gi';
-import { MdOutlinePendingActions } from 'react-icons/md';
+'use client';
 
-import { Button } from '@/components/ui/button';
+import { motion } from 'framer-motion';
+
+import {
+  HeroHighlight,
+  Highlight,
+} from '@/components/Home/HeroSection/hero-highlight';
+import { Config } from '@/lib/config';
 
 export default function HeroSection() {
   return (
-    <div className="my-4 flex h-screen w-full flex-1 flex-col items-center justify-center gap-4">
-      <h1 className="px-8 py-4 text-center text-4xl">
-        A marketplace where selling or buying items gets easier
-      </h1>
-      <div className="space-x-3">
-        <Link href={'/marketplace'}>
-          <Button variant={'outline'} className="w-36">
-            Buy on Claimit
-          </Button>
-        </Link>
-        <Link href={'/auctions/create'}>
-          <Button variant={'default'} className="w-36">
-            Sell on Claimit
-          </Button>
-        </Link>
-      </div>
-      <div className="mt-10 grid grid-cols-2 gap-3 md:grid-cols-4">
-        <Button
-          variant={'outline'}
-          className="flex h-36 w-36 flex-col space-x-2"
-        >
-          <div className="flex gap-2">
-            <TrendingUp className="stroke-emerald-500" />
-            100
+    <HeroHighlight>
+      <motion.h1
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: [20, -5, 0],
+        }}
+        transition={{
+          duration: 0.5,
+          ease: [0.4, 0.0, 0.2, 1],
+        }}
+        className="mx-auto max-w-3xl px-4 text-center text-2xl font-bold leading-relaxed text-neutral-700 dark:text-white md:text-4xl lg:text-5xl lg:leading-snug "
+      >
+        <div className="flex flex-col">
+          <p>
+            On {Config.APP_NAME}, selling or buying{' '}
+            <Highlight className="text-black dark:text-white">
+              antique items
+            </Highlight>{' '}
+            is very easy.
+          </p>
+          <div>
+            <button className="group relative inline-block cursor-pointer rounded-full bg-slate-800 p-px text-xs font-semibold leading-6 text-white no-underline  shadow-2xl shadow-zinc-900">
+              <span className="absolute inset-0 overflow-hidden rounded-full">
+                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+              </span>
+              <div className="relative z-10 flex items-center space-x-2 rounded-full bg-zinc-950 px-4 py-0.5 ring-1 ring-white/10 ">
+                <span>Explore More</span>
+                <svg
+                  fill="none"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  width="16"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.75 8.75L14.25 12L10.75 15.25"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                  />
+                </svg>
+              </div>
+              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+            </button>
           </div>
-          Trending auctions
-        </Button>
-        <Button
-          variant={'outline'}
-          className="flex h-36 w-36 flex-col space-x-2 p-10"
-        >
-          <div className="flex gap-2">
-            <MdOutlinePendingActions size={18} className="fill-orange-500" />
-            100
-          </div>
-          Auctions ending
-          <br /> soon
-        </Button>
-        <Button
-          variant={'outline'}
-          className="flex h-36 w-36 flex-col space-x-2"
-        >
-          <div className="flex gap-2">
-            <GiConfirmed size={18} className="fill-sky-500" />
-            100
-          </div>
-          Auctions sold
-        </Button>
-        <Button
-          variant={'outline'}
-          className="flex h-36 w-36 flex-col space-x-2"
-        >
-          <div className="flex gap-2">
-            <FaUser className="fill-violet-500" />
-            100
-          </div>
-          Registered users
-        </Button>
-      </div>
-    </div>
+        </div>
+      </motion.h1>
+    </HeroHighlight>
   );
 }
